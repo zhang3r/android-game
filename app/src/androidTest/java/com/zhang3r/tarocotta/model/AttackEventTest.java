442b360fa7f478cdf33cb676e43ecec362d094da
zhang3r <zhang3r@gatech.edu>
2015-12-19 12:25:05 -0500

    adding attack event

---
+++ app/src/androidTest/java/com/zhang3r/tarocotta/model/AttackEventTest.java
@@ -0,0 +1,13 @@
+package com.zhang3r.tarocotta.model;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Zhang3r on 12/18/2015.
+ */
+public class AttackEventTest extends TestCase {
+
+    public void testCalcuateDMG() throws Exception {
+
+    }
+}
--- app/src/main/java/com/zhang3r/tarocotta/model/AttackEvent.java
+++ app/src/main/java/com/zhang3r/tarocotta/model/AttackEvent.java
@@ -5,6 +5,9 @@ import android.util.Log;
 import com.zhang3r.tarocotta.constants.IGameConstants;
 import com.zhang3r.tarocotta.constants.ILogConstants;
 import com.zhang3r.tarocotta.model.army.Army;
+import com.zhang3r.tarocotta.model.maps.GameMap;
+import com.zhang3r.tarocotta.model.tiles.terrain.BaseTerrain;
+import com.zhang3r.tarocotta.model.tiles.terrain.TerrainFactory;
 import com.zhang3r.tarocotta.model.tiles.units.BaseUnit;
 import com.zhang3r.tarocotta.model.tiles.units.Impl.RangedAttackImpl;
 
@@ -17,7 +20,30 @@ public class AttackEvent {
     }
     public void calcuateDMG(){
         //get terrain
+        int attackerTerIndex =GameMap.getGameMap().getGrid()[attacker.getPosition().getX()][attacker.getPosition().getY()];
+        int defenderTerIndex =GameMap.getGameMap().getGrid()[defender.getPosition().getX()][defender.getPosition().getY()];
+        BaseTerrain attackerTerr = null;
+        BaseTerrain defenderTerr = null;
         //calculate dmg
+        int dmg = attackerDMG(attacker, defender, attackerTerr, defenderTerr );
+        int recoil = attackerDMG(attacker, defender, attackerTerr, defenderTerr );
         //apply dmg
+        defender.getStats().setHitPoints(defender.getStats().getHitPoints() - dmg);
+        if(defender.getStats().getHitPoints()<=0){
+            defender.setUnitState(IGameConstants.UnitState.DEAD);
+        }
+        attacker.getStats().setHitPoints(attacker.getStats().getHitPoints() - recoil);
+        if(attacker.getStats().getHitPoints()<=0){
+            attacker.setUnitState(IGameConstants.UnitState.DEAD);
+        }
     }
+
+    private int attackerDMG(BaseUnit attacker, BaseUnit defender, BaseTerrain attackTerrain, BaseTerrain defenderTerrain){
+        return 0;
+    }
+    private int attackerRecoil(BaseUnit attacker, BaseUnit defender, BaseTerrain attackTerrain, BaseTerrain defenderTerrain){
+        return 0;
+    }
+
+
 }
